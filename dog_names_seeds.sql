01b3dd95e7ce8c0e373982cd4bdf505b297e04bf
Alli Cummings <[A[D>
2016-05-19 18:46:16 -0400

    add sql seeds

--- dog_names_seeds.sql
+++ dog_names_seeds.sql
@@ -2,12 +2,12 @@ DROP TABLE IF EXISTS dogs;
 
 CREATE TABLE dogs (
     id SERIAL PRIMARY KEY,
-    Zip_Code INTEGER,
+    Zip_Code VARCHAR,
     Dog_Name VARCHAR,
     Animal_Gender VARCHAR
 );
 
 COPY dogs
     (Zip_Code,Dog_Name,Animal_Gender)
-FROM '/Users/Allison/Dropbox (Personal)/General Assembly/hood_matcher/data_sets/Directory_Of_Toilets_In_Public_Parks.csv'
+FROM '/Users/Allison/Dropbox (Personal)/General Assembly/hood_matcher/data_sets/Dog_Names.csv'
     DELIMITER ',' CSV;
