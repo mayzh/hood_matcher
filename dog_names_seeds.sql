7482c85adfc0c90fcee311afe19c0dbaa6b34494
Alli Cummings <[A[D>
2016-05-19 20:51:17 -0400

    fix dog names seeds with neighborhood names

--- dog_names_seeds.sql
+++ dog_names_seeds.sql
@@ -3,11 +3,12 @@ DROP TABLE IF EXISTS dogs;
 CREATE TABLE dogs (
     id SERIAL PRIMARY KEY,
     Zip_Code VARCHAR,
+    Neighborhood VARCHAR,
     Dog_Name VARCHAR,
     Animal_Gender VARCHAR
 );
 
 COPY dogs
-    (Zip_Code,Dog_Name,Animal_Gender)
+    (Zip_Code,Neighborhood,Dog_Name,Animal_Gender)
 FROM '/Users/Allison/Dropbox (Personal)/General Assembly/hood_matcher/data_sets/Dog_Names.csv'
     DELIMITER ',' CSV;
