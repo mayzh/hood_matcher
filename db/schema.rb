9280d9aadec977011baab3b88366b29f91497dd9
Alli Cummings <[A[D>
2016-05-23 14:25:22 -0400

    fix rails app

--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160523004448) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "neighborhoods", force: :cascade do |t|
-    t.string   "name"
-    t.string   "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20160523171010) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
-    t.string   "password_digest"
     t.string   "username"
+    t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
