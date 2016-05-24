607afdfa123fee87b542bf20a68065f8f80bc7fd
May Zhang <mayze863@gmail.com>
2016-05-24 17:13:02 -0400

    add styling tweaks

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,24 +1,19 @@
 (function() {
   console.log('jquery ready')
-/*!
- * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
 
-  // jQuery to collapse the navbar on scroll
+  // collapse the navbar on scroll
   function collapseNavbar() {
       if ($(".navbar").offset().top > 50) {
           $(".navbar-fixed-top").addClass("top-nav-collapse");
       } else {
           $(".navbar-fixed-top").removeClass("top-nav-collapse");
       }
-  } // end of collapseNavbar
+  }
 
   $(window).scroll(collapseNavbar);
   $(document).ready(collapseNavbar);
 
-  // jQuery for page scrolling feature - requires jQuery Easing plugin
+  // page scrolling feature
   $(function() {
       $('a.page-scroll').bind('click', function(event) {
           var $anchor = $(this);
@@ -37,6 +32,6 @@
   });
 
 
-})(); //end of anonymous wrapper function
+})();
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   resources :users
 
+  get '/quiz'
+
 end
