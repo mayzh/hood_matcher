6c14bbbcb1a2bc6bcaff2e9e9722b14c07462850
Alli Cummings <[A[D>
2016-05-25 08:59:05 -0400

    add more matchiness to googleAPI.js

--- app/assets/javascripts/googleAPI.js
+++ app/assets/javascripts/googleAPI.js
@@ -10,7 +10,7 @@ var soho = '75 Wooster St, New York, NY 10012';
 var lowerEast = '268 E Broadway, New York, NY 10002';
 var greenwich = '55 W 8th St, New York, NY 10011';
 var eastVillage = '153 Avenue A, New York, NY 10009';
-var chelsea = '367 W 26th St, New York, NY 10001';
+var chelsea = 'chelsea, new york ny';
 var gramercy = '225 3rd Ave, New York, NY 10003';
 var murrayHill = '528 3rd Ave, New York, NY 10016';
 var midtownEast = '249 E 50th St, New York, NY 10022';
@@ -62,18 +62,15 @@ var heatmapAJAX = function() {
       // gets Latitude
       var lng = data['results'][0]['geometry']['location']['lng'];
       var getPoints = function() {
-        //do loop adds the heatmap dot over and over to same place up to the value of the multipler assigned by response
-        var count = 0;
-        do {
-          count++;
-          return [
-            new google.maps.LatLng(lati, lng)
-          ]
-        } while (count <= multiplier);
+        //adds the heatmap dot with weight based on the value of the multipler assigned by response
+        return [
+          {location: new google.maps.LatLng(lati, lng), weight: multiplier},
+        ]
       } //end of getPoints function
       heatmap = new google.maps.visualization.HeatmapLayer({
           data: getPoints(),
-          map: map
+          map: map,
+          radius: 20
         });
       $('#map').append(map);
     } //end of success function
@@ -92,9 +89,6 @@ var heatmapAJAX = function() {
   }
   } // end of hoodname for loop
 } //end of heatmapAJAX
-
-
-
 
 //function to add initial map to the page
 $.ajax({
@@ -125,8 +119,8 @@ $("input[name*='q1']").click(function(e) {
     case 'q1a':
       results.upperEastSide = 5;
       results.upperWestSide = 4.5;
-      results.soho = 2.5;
-      results.eastVillage = 2.5;
+      // results.soho = 2.5;
+      // results.eastVillage = 2.5;
       results.midtownWest = 2;
       results.chelsea = 2;
       results.inwood = 1.5;
@@ -140,8 +134,8 @@ $("input[name*='q1']").click(function(e) {
     case 'q1b':
       results.upperEastSide = 5;
       results.upperWestSide = 4;
-      results.harlem = 2.5;
-      results.eastVillage = 2.5;
+      // results.harlem = 2.5;
+      // results.eastVillage = 2.5;
       results.soho = 2;
       results.chelsea = 2;
       results.midtownWest = 2;
@@ -159,8 +153,8 @@ $("input[name*='q1']").click(function(e) {
       results.upperWestSide = 5;
       results.upperEastSide = 4.5;
       results.chelsea = 3;
-      results.soho = 2.5;
-      results.midtownWest = 2.5;
+      // results.soho = 2.5;
+      // results.midtownWest = 2.5;
       results.midtownEast = 2;
       results.inwood = 2;
       results.eastVillage = 2;
@@ -244,8 +238,8 @@ $("input[name*='q2']").click(function(e) {
       results.morningSideHts = 3.5;
       results.inwood = 3.5;
       results.midtownWest = 3.5;
-      results.greenwich = 2.5;
-      results.eastHarlem = 2.5;
+      // results.greenwich = 2.5;
+      // results.eastHarlem = 2.5;
       results.gramercy = 1;
       results.chelsea = 1;
       results.murrayHill = 1;
@@ -262,8 +256,8 @@ $("input[name*='q2']").click(function(e) {
       results.murrayHill = 4;
       results.chelsea = 4;
       results.gramercy = 4;
-      results.eastHarlem = 2.5;
-      results.greenwich = 2.5;
+      // results.eastHarlem = 2.5;
+      // results.greenwich = 2.5;
       results.midtownWest = 1.5;
       results.inwood = 1.5;
       results.morningSideHts = 1.5;
@@ -273,7 +267,7 @@ $("input[name*='q2']").click(function(e) {
       break;
   }
   // console.log('the result is ' + result + ' with a multiplier of ' + multiplier);
-  heatmapAJAX(result, multiplier);
+  heatmapAJAX();
 })//end of click function q2 **************************
 
 //**********QUESTION 3***********
@@ -287,9 +281,9 @@ $("input[name*='q3']").click(function(e) {
       results.upperEastSide = 4;
       results.inwood = 3.5;
       results.eastHarlem = 3.5;
-      results.eastVillage = 2.5;
-      results.greenwich = 2.5;
-      results.lowerEast = 2.5;
+      // results.eastVillage = 2.5;
+      // results.greenwich = 2.5;
+      // results.lowerEast = 2.5;
       results.midtownWest = 2;
       results.financialDist = 1.5;
       results.gramercy = 1.5;
@@ -309,16 +303,16 @@ $("input[name*='q3']").click(function(e) {
       results.gramercy = 3.5;
       results.financialDist = 3.5;
       results.midtownWest = 3;
-      results.lowerEast = 2.5;
-      results.greenwich = 2.5;
-      results.eastVillage = 2.5;
+      // results.lowerEast = 2.5;
+      // results.greenwich = 2.5;
+      // results.eastVillage = 2.5;
       results.eastHarlem = 1.5;
       results.inwood = 1.5;
       results.upperEastSide = 1;
       break;
   }
   // console.log('the result is ' + result + ' with a multiplier of ' + multiplier);
-  heatmapAJAX(result, multiplier);
+  heatmapAJAX();
 })//end of click function q3 **************************
 
 //**********QUESTION 4***********
@@ -337,8 +331,8 @@ $("input[name*='q4']").click(function(e) {
       results.gramercy = 3;
       results.tribeca = 3;
       results.lowerEast = 3;
-      results.chinaTown = 2.5;
-      results.upperEastSide =2.5;
+      // results.chinaTown = 2.5;
+      // results.upperEastSide =2.5;
       results.harlem = 2;
       results.financialDist = 2;
       results.upperWestSide = 2;
@@ -355,9 +349,9 @@ $("input[name*='q4']").click(function(e) {
       results.harlem = 3.5;
       results.upperEastSide = 3;
       results.chinaTown = 3;
-      results.lowerEast = 2.5;
-      results.tribeca = 2.5;
-      results.gramercy = 2.5;
+      // results.lowerEast = 2.5;
+      // results.tribeca = 2.5;
+      // results.gramercy = 2.5;
       results.chelsea = 2;
       results.greenwich = 1.5;
       results.eastVillage = 1;
@@ -369,7 +363,7 @@ $("input[name*='q4']").click(function(e) {
   }
   // console.log('the result is ' + result + ' with a multiplier of ' + multiplier);
 
-  heatmapAJAX(result, multiplier);
+  heatmapAJAX();
 })//end of click function q4 **************************
 
 // NUMBER 5 WHAT IS YOUR FAV CUISINE?????
@@ -377,25 +371,74 @@ $("input[name*='q5']").click(function(e){
   var result;
 //switch case to assign a neighborhood and multiper to be added to heatmap depending on which answer is selected.
   switch($('input[name=q5]:checked').attr('id')){
-    case 'q5a':
-      result = 'upperEastSide';
-      multiplier = 10;
+    case 'q5a': // french
+      results.upperEastSide = 5;
+      results.midtownWest = 4.5;
+      results.greenwich = 4;
+      // results.midtownEast = 2.5;
+      // results.lowerEast = 2.5;
+      results.upperWestSide = 2;
+      results.soho = 2;
+      results.murrayHill = 1.5;
+      results.eastVillage = 1.5;
+      results.tribeca = 1.5;
+      results.chelsea = 1;
+      results.gramercy = 1;
+      reesults.financialDist = 1;
+      results.harlem = 1;
+      results.harlem = 0.5
       break;
-    case 'q5b':
-      result = 'chinaTown';
-      multiplier = 10;
+    case 'q5b': // chinese
+      results.chinaTown = 5;
+      results.midtownEast = 3;
+      // results.upperEastSide = 2.5;
+      // results.midtownWest = 2.5;
+      // results.lowerEast = 2.5;
+      // results.eastVillage = 2.5;
+      greenwich = 2;
+      results.chelsea = 2;
+      results.upperWestSide = 1.5;
+      results.murrayHill = 1;
+      results.morningSideHts = 1;
+      results.tribeca = 0.5;
+      results.gramercy = 0.5;
+      results.financialDist = 0.5;
       break;
-    case 'q5c':
-      result = 'midtownWest';
-      multiplier = 10;
+    case 'q5c': // burgers
+      results.midtownWest = 5;
+      results.greenwich = 5;
+      results.financialDist = 5;
+      results.upperEastSide = 4;
+      results.upperWestSide = 4;
+      results.murrayHill = 4;
+      results.eastVillage = 3;
+      results.chelsea = 3;
+      results.midtownEast = 2;
+      results.soho = 2;
+      results.gramercy = 0.5;
+      results.harlem = 0.5;
       break;
-    case 'q5d':
-      result = 'greenwich';
-      multiplier = 10;
+    case 'q5d': // mexican
+      results.greenwich = 5;
+      results.midtownEast = 4;
+      results.chelsea = 4;
+      results.midtownWest = 3.5;
+      results.east = 3.5;
+      resuls.upperEastSide = 3;
+      results.gramercy = 3;
+      results.murrayHill = 3;
+      results.upperWestSide = 2.5;
+      results.lowerEast = 2;
+      results.soho = 1;
+      results.tribeca = 1;
+      results.financialDist = 1;
+      results.eastHarlem = 0.5;
+      results.chinaTown = 0.5;
+      results.morningSideHts = 0.5;
       break;
   }
   // console.log('the result is ' + result + ' with a multiplier of ' + multiplier);
-  heatmapAJAX(result, multiplier);
+  heatmapAJAX();
 })//end of click function q5 **************************
 
 // NUMBER 6 WHO'S YOUR FAVORITE SPIRIT ANIMAL?
@@ -403,32 +446,26 @@ $("input[name*='q6']").click(function(e) {
   var result;
   switch($('input[name=q6]:checked').attr('id')){
     case 'q6a':
-      result = 'morningSideHts';
-      multiplier = 10;
+      results.morningSideHts = 5;
       break;
     case 'q6b':
-      result = 'upperEastSide';
-      multiplier = 10;
+      results.upperEastSide = 5;
       break;
     case 'q6c':
-      result = 'upperWestSide';
-      multiplier = 10;
+      results.upperWestSide = 5;
       break;
     case 'q6d':
-      result = 'greenwich';
-      multiplier = 10;
+      results.greenwich = 5;
       break;
     case 'q6e':
-      result = 'lowerEast';
-      multiplier = 10;
+      results.lowerEast = 5;
       break;
     case 'q6f':
-      result = 'eastVillage';
-      multiplier = 10;
+      results.eastVillage = 5;
       break;
   }
   // console.log('the result is ' + result + ' with a multiplier of ' + multiplier);
-  heatmapAJAX(result, multiplier);
+  heatmapAJAX();
 })//end of click function q6 **************************
 
 // NUMBER 7 FAVORITE SONG ON JUKEBOX?
@@ -436,48 +473,42 @@ $("input[name*='q7']").click(function(e){
   var result;
   switch($('input[name=q7]:checked').attr('id')){
     case 'q7a':
-      result = 'eastHarlem'
-      multiplier = 10;
+      results.eastHarlem = 5;
       break;
     case 'q7b':
-      result = 'upperEastSide';
-      multiplier = 10;
+      results.upperEastSide = 5;
+      results.upperWestSide = 5;
+      results.chelsea = 5;
+      results.midtownEast = 5;
+      results.tribeca = 5;
       break;
     case 'q7c':
-      result = 'upperWestSide';
-      multiplier = 5;
+      results.upperWestSide = 2.5;
       break;
     case 'q7d':
-      result = 'midtownWest';
-      multiplier = 10;
+      results.midtownWest = 5;
       break;
     case 'q7e':
-      result = 'murrayHill';
-      multiplier = 10;
+      results.murrayHill = 5;
       break;
     case 'q7f':
-      result = 'gramercy';
-      multiplier = 10;
+      results.gramercy = 5;
       break;
     case 'q7g':
-      result = 'lowerEast';
-      multiplier = 10;
+      results.lowerEast = 5;
       break;
     case 'q7h':
-      result = 'chinaTown';
-      multiplier = 10;
+      results.chinaTown = 5;
       break;
     case 'q7i':
-      result = 'morningSideHts';
-      multiplier = 10;
+      results.morningSideHts = 5;
       break;
     case 'q7j':
-      result = 'financialDist';
-      multiplier = 10;
+      results.financialDist = 5;
       break;
   }
   // console.log('the result is ' + result + ' with a multiplier of ' + multiplier);
-  heatmapAJAX(result, multiplier);
+  heatmapAJAX();
 })//end of click function q7 **************************
 
 // **************** QUESTION 8
@@ -486,102 +517,94 @@ $("input[name*='q8']").click(function(e) {
 
   switch($('input[name=q8]:checked').attr('id')){
     case 'q8a':
-      result = 'upperWestSide';
-      multiplier = 10;
+      results.upperWestSide = 5;
       break;
     case 'q8b':
-      result = 'upperEastSide';
-      multiplier = 10;
+      results.upperEastSide = 5;
       break;
     case 'q8c':
-      result = 'eastVillage';
-      multiplier = 10;
+      results.eastVillage = 5;
       break;
     case 'q8d':
-      result = 'lowerEast';
-      multiplier = 10;
+      results.lowerEast = 5;
       break;
     case 'q8e':
-      result = 'tribeca';
-      multiplier = 10;
+      results.tribeca = 5;
       break;
     case 'q8f':
-      result = 'financialDist';
-      multiplier = 10;
+      results.financialDist = 5;
       break;
     case 'q8g':
-      result = 'murrayHill';
-      multiplier = 10;
+      results.murrayHill = 5;
       break;
     case 'q8h':
-      result = 'eastHarlem';
-      multiplier = 10;
+      results.eastHarlem = 5;
       break;
     case 'q8i':
-      result = 'gramercy';
-      multiplier = 10;
+      results.gramercy = 5;
       break;
     case 'q8j':
-      result = 'morningSideHts';
-      multiplier = 10;
+      results.morningSideHts = 5;
       break;
     case 'q8k':
-      result = 'midtownWest';
-      multiplier = 10;
+      results.midtownWest = 5;
       break;
   }
-  // console.log('the result is ' + result + ' with a multiplier of ' + multiplier);
-
-   heatmapAJAX(result, multiplier);
+   heatmapAJAX();
 })//end of click function q8 *********************
 
 //********************* QUESTION 9
 $("input[name*='q9']").click(function(e) {
-
   var result;
-
   switch($('input[name=q9]:checked').attr('id')){
     case 'q9a':
-      result = 'inwood';
-      multiplier = 10;
+      results.inwood = 5;
+      results.eastHarlem = 5;
+      results.harlem = 4;
       break;
     case 'q9b':
-      result = 'morningSideHts';
-      multiplier = 10;
+      results.morningSideHts = 5;
+      results.upperWestSide = 5;
+      results.upperEastSide = 5;
       break;
     case 'q9c':
-      result = 'midtownEast';
-      multiplier = 10;
+      results.midtownEast = 5;
+      results.midtownWest = 5;
+      results.chelsea = 5;
+      results.eastVillage = 5;
+      results.greenwich = 5;
+      results.murrayHill = 3.5;
+      results.lowerEast = 3.5;
+      results.financialDist = 3.5;
       break;
-
   }
-  // console.log('the result is ' + result + ' with a multiplier of ' + multiplier);
-  heatmapAJAX(result, multiplier);
+  heatmapAJAX();
 })//end of click function q9************************
 
 //********************* QUESTION 10
 $("input[name='q10']").click(function(e) {
-
   var result;
-
   switch($('input[name=q10]:checked').attr('id')){
-    case 'q10a':
-      result = 'eastHarlem';
-      multiplier = 10;
+    case 'q10a': // rarely
+      results.eastHarlem = 5;
+      results.upperWestSide = 4;
       break;
-    case 'q10b':
-      result = 'murrayHill';
-      multiplier = 5;
+    case 'q10b': //few times a week
+      results.murrayHill = 3;
+      results.upperEastSide = 3;
+      results.lowerEast = 3;
+      results.soho = 3;
+      results.tribeca = 3;
+      results.financialDist = 3;
       break;
-    case 'q10c':
-      result = 'midtownWest';
-      multiplier = 10;
+    case 'q10c': // all the time
+      results.midtownWest = 5;
+      results.greenwich = 4;
+      results.chelsea = 4;
+      results.gramercy = 5;
       break;
-
   }
-  // console.log('the result is ' + result + ' with a multiplier of ' + multiplier);
-
-  heatmapAJAX(result, multiplier);
+  heatmapAJAX();
   //go thru userResults to find the neighborhood with highest number of heat dots:
   var hoodWithMostDots = Object.keys(userResults).reduce(function(a, b){ return userResults[a] > userResults[b] ? a : b });
 // var hoodWithMostDots = userResults.slice(0).sort(function(x, y) { return y.number - x.number })[0];
